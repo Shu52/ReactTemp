d80c3bd64d8b1abc0f678ad624a11fb5ff1f62d9
Shu <shuaib.a.sajid@accenture.com>
2021-02-25 09:43:48 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      celsius: null,
+      fahrenheit: null,
+      random: null
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,      
+    });
+      if (e.target.name === 'celsius'){      
+        this.setState({
+          fahrenheit:(e.target.value * 9 / 5) + 32   
+        });
+    }
+      if (e.target.name === 'fahrenheit'){        
+        this.setState({
+          celsius:(e.target.value - 32) * 5 / 9   
+        });
+    }
+  };
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+      type="number"
+      name="celsius"
+      placeholder="Enter Degrees in Celsius"
+      onChange={this.handleChange}
+      value={this.state.celsius} />
+      <input
+      type="number"
+      name="fahrenheit"
+      placeholder="Enter Degrees in fahrenheit"
+      onChange={this.handleChange}
+      value={this.state.fahrenheit} />
     </div>
   );
+}
 }
 
 export default App;
